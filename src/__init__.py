98fa76f816248ec0a946682387728c0c78cca115
Omar Duran <omaryesiduran@gmail.com>
2022-08-09 15:31:53 +0200

    DOC: black

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,8 @@
-
 # import os
 
 from . import geometry
 
-__all__ = ['geometry', 'integration', 'mesh', 'postprocess']
+__all__ = ["geometry", "integration", "mesh", "postprocess"]
 
 __all__.extend(geometry.__all__)
 __all__.extend(integration.__all__)
@@ -16,7 +15,6 @@ submodules = [geometry, integration, mesh, postprocess]
 try:
     nfempy_dir = os.path.dirname(__file__)
 except:
-    nfempy_dir = ''
+    nfempy_dir = ""
 
-__version__ = '0.0.0'
-
+__version__ = "0.0.0"
