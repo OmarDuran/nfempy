BUG: Gradients of vector functions were not properly expanded

src/basis/finite_element.py
@@ -133,7 +133,7 @@ class FiniteElement:
         else:
             shape = phi_tab.shape
             new_shape = (shape[0], shape[1], shape[2], phi_mapped.shape[2])
-            phi_tab = np.resize(phi_tab, new_shape)
+            phi_tab = np.insert(phi_tab, 2, 0, axis=3)
             phi_tab[0] = phi_mapped
 
         phi_tab = permute_and_transform(phi_tab, self.data)