e322ad6b55161a903a0bc318ce72aeead0d55c91
Omar Duran <omaryesiduran@gmail.com>
2023-12-27 22:48:17 +0100

    DEV: Partial vectorization on weak forms

--- src/weak_forms/lce_dual_weak_form.py
+++ src/weak_forms/lce_dual_weak_form.py
@@ -165,6 +165,378 @@ class LCEDualWeakForm(WeakForm):
 
                     A_mh = (1.0 / f_gamma(xv[0], xv[1], xv[2])) * mh
 
+                    Gamma_outer = th * np.array([[0.0, -1.0], [1.0, 0.0]])
+                    S_cross = np.array([[sh[1, 0] - sh[0, 1]]])
+
+                else:
+                    c = 0
+                    a_sx = alpha[:, c : n_s_dof + c : s_components]
+                    a_ux = alpha[
+                        :,
+                        n_s_dof
+                        + n_m_dof
+                        + c : n_s_dof
+                        + n_m_dof
+                        + n_u_dof
+                        + c : u_components,
+                    ]
+                    a_mx = alpha[:, n_s_dof + c : n_s_dof + n_m_dof + c : m_components]
+                    a_tx = alpha[
+                        :,
+                        n_s_dof
+                        + n_m_dof
+                        + n_u_dof
+                        + c : n_s_dof
+                        + n_m_dof
+                        + n_u_dof
+                        + n_t_dof
+                        + c : t_components,
+                    ]
+
+                    c = 1
+                    a_sy = alpha[:, c : n_s_dof + c : s_components]
+                    a_uy = alpha[
+                        :,
+                        n_s_dof
+                        + n_m_dof
+                        + c : n_s_dof
+                        + n_m_dof
+                        + n_u_dof
+                        + c : u_components,
+                    ]
+                    a_my = alpha[:, n_s_dof + c : n_s_dof + n_m_dof + c : m_components]
+                    a_ty = alpha[
+                        :,
+                        n_s_dof
+                        + n_m_dof
+                        + n_u_dof
+                        + c : n_s_dof
+                        + n_m_dof
+                        + n_u_dof
+                        + n_t_dof
+                        + c : t_components,
+                    ]
+
+                    c = 2
+                    a_sz = alpha[:, c : n_s_dof + c : s_components]
+                    a_uz = alpha[
+                        :,
+                        n_s_dof
+                        + n_m_dof
+                        + c : n_s_dof
+                        + n_m_dof
+                        + n_u_dof
+                        + c : u_components,
+                    ]
+                    a_mz = alpha[:, n_s_dof + c : n_s_dof + n_m_dof + c : m_components]
+                    a_tz = alpha[
+                        :,
+                        n_s_dof
+                        + n_m_dof
+                        + n_u_dof
+                        + c : n_s_dof
+                        + n_m_dof
+                        + n_u_dof
+                        + n_t_dof
+                        + c : t_components,
+                    ]
+
+                    sx_h = a_sx @ s_phi_tab[0, i, :, 0:dim]
+                    sy_h = a_sy @ s_phi_tab[0, i, :, 0:dim]
+                    sz_h = a_sz @ s_phi_tab[0, i, :, 0:dim]
+
+                    mx_h = a_mx @ m_phi_tab[0, i, :, 0:dim]
+                    my_h = a_my @ m_phi_tab[0, i, :, 0:dim]
+                    mz_h = a_mz @ m_phi_tab[0, i, :, 0:dim]
+
+                    ux_h = a_ux @ u_phi_tab[0, i, :, 0:dim]
+                    uy_h = a_uy @ u_phi_tab[0, i, :, 0:dim]
+                    uz_h = a_uz @ u_phi_tab[0, i, :, 0:dim]
+
+                    tx_h = a_tx @ t_phi_tab[0, i, :, 0:dim]
+                    ty_h = a_ty @ t_phi_tab[0, i, :, 0:dim]
+                    tz_h = a_tz @ t_phi_tab[0, i, :, 0:dim]
+
+                    uh = VecValDer(
+                        np.hstack((ux_h.val, uy_h.val, uz_h.val)),
+                        np.hstack((ux_h.der, uy_h.der, uz_h.der)),
+                    )
+
+                    th = VecValDer(
+                        np.hstack((tx_h.val, ty_h.val, tz_h.val)),
+                        np.hstack((tx_h.der, ty_h.der, tz_h.der)),
+                    )
+
+                    sh = VecValDer(
+                        np.vstack((sx_h.val, sy_h.val, sz_h.val)),
+                        np.vstack((sx_h.der, sy_h.der, sz_h.der)),
+                    )
+
+                    mh = VecValDer(
+                        np.vstack((mx_h.val, my_h.val, mz_h.val)),
+                        np.vstack((mx_h.der, my_h.der, mz_h.der)),
+                    )
+
+                    # Stress decomposition
+                    Symm_sh = 0.5 * (sh + sh.T)
+                    Skew_sh = 0.5 * (sh - sh.T)
+
+                    tr_s_h = VecValDer(sh.val.trace(), sh.der.trace())
+                    A_sh = (1.0 / 2.0 * f_mu(xv[0], xv[1], xv[2])) * (
+                        Symm_sh
+                        - (
+                            f_lambda(xv[0], xv[1], xv[2])
+                            / (
+                                2.0 * f_mu(xv[0], xv[1], xv[2])
+                                + dim * f_lambda(xv[0], xv[1], xv[2])
+                            )
+                        )
+                        * tr_s_h
+                        * Imat
+                    ) + (1.0 / 2.0 * f_kappa(xv[0], xv[1], xv[2])) * Skew_sh
+
+                    A_mh = (1.0 / f_gamma(xv[0], xv[1], xv[2])) * mh
+
+                    Gamma_outer = np.array(
+                        [
+                            [0.0 * th[0, 0], -th[0, 2], +th[0, 1]],
+                            [+th[0, 2], 0.0 * th[0, 0], -th[0, 0]],
+                            [-th[0, 1], +th[0, 0], 0.0 * th[0, 0]],
+                        ]
+                    )
+
+                    S_cross = np.array(
+                        [
+                            [
+                                sh[2, 1] - sh[1, 2],
+                                sh[0, 2] - sh[2, 0],
+                                sh[1, 0] - sh[0, 1],
+                            ]
+                        ]
+                    )
+
+                equ_1_integrand = (
+                    (s_phi_tab[0, i, :, 0:dim] @ A_sh.T)
+                    # + (div_tau.T @ uh)
+                    - (s_phi_tab[0, i, :, 0:dim] @ Gamma_outer.T)
+                )
+                equ_2_integrand = (m_phi_tab[0, i, :, 0:dim] @ A_mh.T)
+                equ_4_integrand = - (
+                    t_phi_tab[0, i, :, 0:dim] @ S_cross
+                )
+
+                multiphysic_integrand = np.zeros((1, n_dof))
+                multiphysic_integrand[:, 0:n_s_dof:1] = (equ_1_integrand).reshape(
+                    (n_s_dof,)
+                )
+                multiphysic_integrand[:, n_s_dof : n_s_dof + n_m_dof : 1] = (
+                    equ_2_integrand
+                ).reshape((n_m_dof,))
+                multiphysic_integrand[
+                    :,
+                    n_s_dof
+                    + n_m_dof
+                    + n_u_dof : n_s_dof
+                    + n_m_dof
+                    + n_u_dof
+                    + n_t_dof : 1,
+                ] = (equ_4_integrand).reshape((n_t_dof,))
+
+                discrete_integrand = (multiphysic_integrand).reshape((n_dof,))
+                el_form += det_jac[i] * omega * discrete_integrand
+
+        r_el, j_el = el_form.val, el_form.der.reshape((n_dof, n_dof))
+
+        # partial vectorization
+        u_phi_s_star_div_det_jac = weights * u_phi_tab[0, :, :, 0].T
+        t_phi_s_star_div_det_jac = weights * t_phi_tab[0, :, :, 0].T
+
+        grad_s_phi_star = s_phi_tab[1: s_phi_tab.shape[0] + 1, :, :, 0:dim]
+        div_tau_star = np.trace(grad_s_phi_star, axis1=0, axis2=3)
+        u_block_outer = u_phi_s_star_div_det_jac @ div_tau_star
+        for uc in range(u_components):
+            ub = uc + n_s_dof + n_m_dof
+            ue = ub + n_u_dof
+            for sc in range(s_components):
+                sb = sc
+                se = sb + n_s_dof
+                if uc != sc:
+                    continue
+
+                j_el[ub:ue:u_components, sb:se:s_components] += u_block_outer
+                j_el[sb:se:s_components, ub:ue:u_components] += u_block_outer.T
+
+        grad_m_phi_star = m_phi_tab[1: m_phi_tab.shape[0] + 1, :, :, 0:dim]
+        div_v_star = np.trace(grad_m_phi_star, axis1=0, axis2=3)
+        t_block_outer = t_phi_s_star_div_det_jac @ div_v_star
+        for tc in range(t_components):
+            tb = tc + n_s_dof + n_m_dof + n_u_dof
+            te = tb + n_t_dof
+            for mc in range(m_components):
+                mb = mc + n_s_dof
+                me = mb + n_m_dof
+                if tc != mc:
+                    continue
+                j_el[tb:te:t_components, mb:me:m_components] += t_block_outer
+                j_el[mb:me:m_components, tb:te:t_components] += t_block_outer.T
+
+        return r_el, j_el
+
+    def evaluate_form_ad(self, element_index, alpha):
+        iel = element_index
+        if self.space is None or self.functions is None:
+            raise ValueError
+
+        s_space = self.space.discrete_spaces["s"]
+        m_space = self.space.discrete_spaces["m"]
+        u_space = self.space.discrete_spaces["u"]
+        t_space = self.space.discrete_spaces["t"]
+
+        f_rhs = self.functions["rhs"]
+        f_lambda = self.functions["lambda"]
+        f_mu = self.functions["mu"]
+        f_kappa = self.functions["kappa"]
+        f_gamma = self.functions["gamma"]
+
+        s_components = s_space.n_comp
+        m_components = m_space.n_comp
+        u_components = u_space.n_comp
+        t_components = t_space.n_comp
+
+        s_data: ElementData = s_space.elements[iel].data
+        m_data: ElementData = m_space.elements[iel].data
+        u_data: ElementData = u_space.elements[iel].data
+        t_data: ElementData = t_space.elements[iel].data
+
+        cell = s_data.cell
+        dim = s_data.dimension
+        points = s_data.quadrature.points
+        weights = s_data.quadrature.weights
+        x = s_data.mapping.x
+        det_jac = s_data.mapping.det_jac
+        inv_jac = s_data.mapping.inv_jac
+
+        # basis
+        s_phi_tab = s_space.elements[iel].evaluate_basis(points)
+        m_phi_tab = m_space.elements[iel].evaluate_basis(points)
+        u_phi_tab = u_space.elements[iel].evaluate_basis(points)
+        t_phi_tab = t_space.elements[iel].evaluate_basis(points)
+
+        n_s_phi = s_phi_tab.shape[2]
+        n_m_phi = m_phi_tab.shape[2]
+        n_u_phi = u_phi_tab.shape[2]
+        n_t_phi = t_phi_tab.shape[2]
+
+        n_s_dof = n_s_phi * s_components
+        n_m_dof = n_m_phi * m_components
+        n_u_dof = n_u_phi * u_components
+        n_t_dof = n_t_phi * t_components
+
+        n_dof = n_s_dof + n_m_dof + n_u_dof + n_t_dof
+        js = (n_dof, n_dof)
+        rs = n_dof
+        j_el = np.zeros(js)
+        r_el = np.zeros(rs)
+
+        # Partial local vectorization
+        f_val_star = f_rhs(x[:, 0], x[:, 1], x[:, 2])
+        u_phi_s_star = det_jac * weights * u_phi_tab[0, :, :, 0].T
+        t_phi_s_star = det_jac * weights * t_phi_tab[0, :, :, 0].T
+
+        # constant directors
+        e1 = np.array([1, 0, 0])
+        e2 = np.array([0, 1, 0])
+        e3 = np.array([0, 0, 1])
+        Imat = np.identity(dim)
+        with ad.AutoDiff(alpha) as alpha:
+            el_form = np.zeros(n_dof)
+            for c in range(u_components):
+                b = c + n_s_dof + n_m_dof
+                e = b + n_u_dof
+                el_form[b:e:u_components] += -1.0 * u_phi_s_star @ f_val_star[c]
+            for c in range(t_components):
+                b = c + n_s_dof + n_m_dof + n_u_dof
+                e = b + n_t_dof
+                el_form[b:e:t_components] += (
+                    -1.0 * t_phi_s_star @ f_val_star[c + u_components]
+                )
+
+            for i, omega in enumerate(weights):
+                xv = x[i]
+                if dim == 2:
+                    c = 0
+                    a_sx = alpha[:, c : n_s_dof + c : s_components]
+                    a_ux = alpha[
+                        :,
+                        n_s_dof
+                        + n_m_dof
+                        + c : n_s_dof
+                        + n_m_dof
+                        + n_u_dof
+                        + c : u_components,
+                    ]
+
+                    a_m = alpha[:, n_s_dof + c : n_s_dof + n_m_dof + c : m_components]
+                    a_t = alpha[
+                        :,
+                        n_s_dof
+                        + n_m_dof
+                        + n_u_dof
+                        + c : n_s_dof
+                        + n_m_dof
+                        + n_u_dof
+                        + n_t_dof
+                        + c : t_components,
+                    ]
+
+                    c = 1
+                    a_sy = alpha[:, c : n_s_dof + c : s_components]
+                    a_uy = alpha[
+                        :,
+                        n_s_dof
+                        + n_m_dof
+                        + c : n_s_dof
+                        + n_m_dof
+                        + n_u_dof
+                        + c : u_components,
+                    ]
+
+                    sx_h = a_sx @ s_phi_tab[0, i, :, 0:dim]
+                    sy_h = a_sy @ s_phi_tab[0, i, :, 0:dim]
+                    ux_h = a_ux @ u_phi_tab[0, i, :, 0:dim]
+                    uy_h = a_uy @ u_phi_tab[0, i, :, 0:dim]
+
+                    mh = a_m @ m_phi_tab[0, i, :, 0:dim]
+                    th = a_t @ t_phi_tab[0, i, :, 0:dim]
+
+                    uh = VecValDer(
+                        np.hstack((ux_h.val, uy_h.val)), np.hstack((ux_h.der, uy_h.der))
+                    )
+
+                    sh = VecValDer(
+                        np.vstack((sx_h.val, sy_h.val)), np.vstack((sx_h.der, sy_h.der))
+                    )
+
+                    # Stress decomposition
+                    Symm_sh = 0.5 * (sh + sh.T)
+                    Skew_sh = 0.5 * (sh - sh.T)
+
+                    tr_s_h = VecValDer(sh.val.trace(), sh.der.trace())
+                    A_sh = (1.0 / 2.0 * f_mu(xv[0], xv[1], xv[2])) * (
+                        Symm_sh
+                        - (
+                            f_lambda(xv[0], xv[1], xv[2])
+                            / (
+                                2.0 * f_mu(xv[0], xv[1], xv[2])
+                                + dim * f_lambda(xv[0], xv[1], xv[2])
+                            )
+                        )
+                        * tr_s_h
+                        * Imat
+                    ) + (1.0 / 2.0 * f_kappa(xv[0], xv[1], xv[2])) * Skew_sh
+
+                    A_mh = (1.0 / f_gamma(xv[0], xv[1], xv[2])) * mh
+
                     grad_s_phi = s_phi_tab[1 : s_phi_tab.shape[0] + 1, i, :, 0:dim]
                     div_tau = np.array([np.trace(grad_s_phi, axis1=0, axis2=2) / det_jac[i]])
 
