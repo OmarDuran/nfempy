7fcd9b0a8d0ab9c700fd94c096573eff1458994a
Omar Duran <omaryesiduran@gmail.com>
2024-03-11 02:06:22 +0100

    MAINT: compress files

--- examples/cosserat_elasticity/reproduce_mfe_cosserat.sh
+++ examples/cosserat_elasticity/reproduce_mfe_cosserat.sh
@@ -8,4 +8,5 @@ echo "Rendering paper figures:: started"
 sh ./render_figures.sh
 echo "Rendering paper figures:: completed"
 
-
+tar -zcvf output_mfe.tar.gz output_example_1/ output_example_2/ output_example_3
+tar -zcvf figures.tar.gz figures/
