095c85e4aba5d46bf660f073620dec3979601c4b
Sundus Iqbal <144384737+Sundus-Iqbal@users.noreply.github.com>
2024-05-07 12:08:48 +0200

    MAINT: arbogast script is operating.

---
+++ examples/advection_diffusion/Arbogast_Weak_Form.py
@@ -0,0 +1,163 @@
+import auto_diff as ad
+import basix
+import numpy as np
+from auto_diff.vecvalder import VecValDer
+from basix import CellType
+
+from basis.element_data import ElementData
+from basis.parametric_transformation import transform_lower_to_higher
+from geometry.compute_normal import normal
+from topology.topological_queries import find_higher_dimension_neighs
+from weak_forms.weak_from import WeakForm
+
+
+class ArbogastDualWeakForm(WeakForm):
+    def evaluate_form(self, element_index, alpha):
+        iel = element_index
+        if self.space is None or self.functions is None:
+            raise ValueError
+
+        mp_space = self.space.discrete_spaces["mp"] #flux for pressure
+        p_space = self.space.discrete_spaces["p"]
+
+        f_rhs_f = self.functions["rhs_f"]
+        f_kappa = self.functions["kappa"]
+        f_porosity = self.functions["porosity"]
+        f_d_phi = self.functions["d_phi"]
+
+
+        mp_components = mp_space.n_comp # mass flux for pressure
+        p_components  = p_space.n_comp
+
+        mp_data: ElementData = mp_space.elements[iel].data
+        p_data: ElementData = p_space.elements[iel].data
+
+        cell = mp_data.cell
+        dim = mp_data.dimension
+        points, weights = self.space.quadrature
+        x, jac, det_jac, inv_jac = mp_space.elements[iel].evaluate_mapping(points)
+
+        # basis
+        mp_phi_tab = mp_space.elements[iel].evaluate_basis(points, jac, det_jac, inv_jac)
+        p_phi_tab = p_space.elements[iel].evaluate_basis(points, jac, det_jac, inv_jac)
+
+        n_mp_phi = mp_phi_tab.shape[2]
+        n_p_phi  = p_phi_tab.shape[2]
+
+        n_mp_dof = n_mp_phi * mp_components
+        n_p_dof  = n_p_phi * p_components
+
+        n_dof = n_mp_dof + n_p_dof
+        js = (n_dof, n_dof)
+        rs = n_dof
+        j_el = np.zeros(js)
+        r_el = np.zeros(rs)
+
+        # Partial local vectorization
+        f_f_val_star = f_rhs_f(x[:, 0], x[:, 1], x[:, 2])
+
+        phi_p_star = det_jac * weights * p_phi_tab[0, :, :, 0].T
+
+        # constant directors
+        e1 = np.array([1, 0, 0])
+        e2 = np.array([0, 1, 0])
+        e3 = np.array([0, 0, 1])
+        with ad.AutoDiff(alpha) as alpha:
+            el_form = np.zeros(n_dof)
+            for c in range(p_components):
+                b = c + n_mp_dof
+                e = b + n_p_dof
+                el_form[b:e:p_components] -= phi_p_star @ f_f_val_star[c].T
+
+            for i, omega in enumerate(weights):
+                xv = x[i]
+                mp_h = alpha[:, 0:n_mp_dof:1] @ mp_phi_tab[0, i, :, 0:dim]
+                mp_h *= 1.0 / (f_d_phi(xv[0], xv[1], xv[2])**2)
+
+                p_h = (
+                    alpha[:, n_mp_dof : n_mp_dof + n_p_dof : 1]
+                    @ p_phi_tab[0, i, :, 0:dim]
+                )
+
+                grad_mp_h = mp_phi_tab[1 : mp_phi_tab.shape[0] + 1, i, :, 0:dim]
+                div_vc_h = np.array(
+                    [[np.trace(grad_mp_h[:, j, :]) / det_jac[i] for j in range(n_mp_dof)]]
+                )
+
+                div_mp_h = alpha[:, 0:n_mp_dof:1] @ div_vc_h.T
+
+                equ_1_integrand = (mp_h @ mp_phi_tab[0, i, :, 0:dim].T) - (p_h @ div_vc_h)
+                equ_2_integrand = div_mp_h @ p_phi_tab[0, i, :, 0:dim].T
+
+                multiphysic_integrand = np.zeros((1, n_dof))
+                multiphysic_integrand[:, 0:n_mp_dof:1] = equ_1_integrand
+                multiphysic_integrand[:, n_mp_dof : n_mp_dof + n_p_dof: 1] = equ_2_integrand
+                discrete_integrand = (multiphysic_integrand).reshape((n_dof,))
+                el_form += det_jac[i] * omega * discrete_integrand
+
+        r_el, j_el = el_form.val, el_form.der.reshape((n_dof, n_dof))
+
+        return r_el, j_el
+
+
+class ArbogastDualWeakFormBCDirichlet(WeakForm):
+    def evaluate_form(self, element_index, alpha):
+        iel = element_index
+        p_D = self.functions["p"]
+
+        mp_space = self.space.discrete_spaces["mp"]
+        mp_components = mp_space.n_comp
+        mp_data: ElementData = mp_space.bc_elements[iel].data
+
+        cell = mp_data.cell
+        points, weights = self.space.bc_quadrature
+        dim = mp_data.dimension
+        x, jac, det_jac, inv_jac = mp_space.bc_elements[iel].evaluate_mapping(points)
+
+        # find high-dimension neigh q space
+        neigh_list = find_higher_dimension_neighs(cell, mp_space.dof_map.mesh_topology)
+        neigh_check_mp = len(neigh_list) > 0
+        assert neigh_check_mp
+        neigh_cell_id = neigh_list[0]
+        neigh_cell_index = mp_space.id_to_element[neigh_cell_id]
+        neigh_element = mp_space.elements[neigh_cell_index]
+        neigh_cell = neigh_element.data.cell
+
+        # compute trace q space
+        mapped_points = transform_lower_to_higher(points, mp_data, neigh_element.data)
+        _, jac_c0, det_jac_c0, inv_jac_c0 = neigh_element.evaluate_mapping(
+            mapped_points
+        )
+        mp_tr_phi_tab = neigh_element.evaluate_basis(
+            mapped_points, jac_c0, det_jac_c0, inv_jac_c0
+        )
+        mp_facet_index = neigh_cell.sub_cells_ids[cell.dimension].tolist().index(cell.id)
+        mp_dof_n_index = neigh_element.data.dof.entity_dofs[cell.dimension][
+            mp_facet_index
+        ]
+
+        n_mp_phi = mp_tr_phi_tab[0, :, mp_dof_n_index, 0:dim].shape[0]
+        n_mp_dof = n_mp_phi * mp_components
+
+        n_dof = n_mp_dof
+        js = (n_dof, n_dof)
+        rs = n_dof
+        j_el = np.zeros(js)
+        r_el = np.zeros(rs)
+
+        # compute normal
+        n = normal(mp_data.mesh, neigh_cell, cell)
+        for c in range(mp_components):
+            b = c
+            e = b + n_mp_dof
+
+            res_block_mp = np.zeros(n_mp_phi)
+            dim = neigh_cell.dimension
+            for i, omega in enumerate(weights):
+                p_D_v = p_D(x[i, 0], x[i, 1], x[i, 2])
+                phi = mp_tr_phi_tab[0, i, mp_dof_n_index, 0:dim] @ n[0:dim]
+                res_block_mp += det_jac[i] * omega * p_D_v[c] * phi
+
+            r_el[b:e:mp_components] += res_block_mp
+
+        return r_el, j_el
---
+++ examples/advection_diffusion/arbogast_mixed_flow_model.py
@@ -0,0 +1,356 @@
+import time
+import math
+import numpy as np
+import scipy
+from petsc4py import PETSc
+
+from basis.element_data import ElementData
+from geometry.domain import Domain
+from geometry.domain_market import build_box_1D, build_box_2D, build_box_3D
+from mesh.conformal_mesher import ConformalMesher
+from mesh.mesh import Mesh
+from postprocess.l2_error_post_processor import l2_error
+from postprocess.projectors import l2_projector
+from postprocess.solution_post_processor import write_vtk_file_with_exact_solution
+from spaces.product_space import ProductSpace
+from Arbogast_Weak_Form import (
+    ArbogastDualWeakForm,
+    ArbogastDualWeakFormBCDirichlet,
+)
+import matplotlib.pyplot as plt
+
+
+def create_product_space(method, gmesh):
+    # FESpace: data
+    mp_k_order = method[1]["mp"][1]
+    p_k_order  = method[1]["p"][1]
+
+    mp_components = 1
+    p_components  = 1
+
+    mp_family = method[1]["mp"][0]
+    p_family  = method[1]["p"][0]
+
+    discrete_spaces_data = {
+        "mp": (gmesh.dimension, mp_components, mp_family, mp_k_order, gmesh),
+        "p" : (gmesh.dimension, p_components, p_family, p_k_order, gmesh),
+    }
+
+    mp_disc_Q = False
+    p_disc_Q  = True
+    discrete_spaces_disc = {
+        "mp": mp_disc_Q,
+        "p" : p_disc_Q,
+    }
+
+    if gmesh.dimension == 1:
+        mp_field_bc_physical_tags = [2, 3]
+    elif gmesh.dimension == 2:
+        mp_field_bc_physical_tags = [2, 3, 4, 5]
+    elif gmesh.dimension == 3:
+        mp_field_bc_physical_tags = [2, 3, 4, 5, 6, 7]
+    else:
+        raise ValueError("Case not available.")
+
+    discrete_spaces_bc_physical_tags = {
+        "mp": mp_field_bc_physical_tags,
+    }
+
+    space = ProductSpace(discrete_spaces_data)
+    space.make_subspaces_discontinuous(discrete_spaces_disc)
+    space.build_structures(discrete_spaces_bc_physical_tags)
+    return space
+
+
+def method_definition(k_order):
+    # lower order convention
+    method_1 = {
+        "mp": ("RT", k_order + 1),
+        "p" : ("Lagrange", k_order),
+    }
+
+    methods = [method_1]
+    method_names = ["mixed_rt"]
+    return zip(method_names, methods)
+
+def two_fields_formulation(method, gmesh, write_vtk_q=False):
+    dim = gmesh.dimension
+
+    fe_space = create_product_space(method, gmesh)
+
+    # Nonlinear solver data
+    n_iterations = 20
+    eps_tol = 1.0e-10
+
+    n_dof_g = fe_space.n_dof
+
+    # Material data as scalars
+    m_kappa = 1.0
+    m_delta = 1.0
+    m_eta  = 1.0
+    m_mu = 1.0
+    r1 = (-3 + np.sqrt(13))/2
+    r2 = (-3 - np.sqrt(13))/2
+    beta = 0.5
+
+
+    def f_porosity(x, y, z):
+        return x**2
+
+    def f_kappa(x, y, z):
+        return f_porosity(x, y, z)**2
+
+    def f_mu(x, y, z):
+        return m_mu
+
+    def f_d_phi(x, y, z):
+        return np.sqrt(f_kappa(x, y, z)/f_mu(x, y, z) )
+
+
+    st = time.time()
+
+    # exact solution
+    if dim == 1:
+
+
+        p_exact = lambda x, y, z: np.array([((-x**beta + 0.5 * (3 + np.sqrt(13)) * x**(0.5*(- 3 + np.sqrt(13))))*beta) / (-1 + beta*(3+beta))])
+        mp_exact = lambda x, y, z: np.array(
+            [
+                ((-x**(0.5*(3 + np.sqrt(13))) + x**(3+beta))*beta)/(-1 + beta*(3+beta)),
+            ]
+        )
+
+        f_rhs = lambda x, y, z: np.array([[(x**beta) * f_porosity(x,y,z)]])
+
+
+    else:
+        raise ValueError("Invalid dimension.")
+
+    m_functions = {
+        "rhs_f": f_rhs,
+        "kappa": f_kappa,
+        "porosity": f_porosity,
+        "d_phi" : f_d_phi,
+    }
+
+    exact_functions = {
+        "mp": mp_exact,
+        "p": p_exact,
+    }
+
+    weak_form = ArbogastDualWeakForm(fe_space)
+    weak_form.functions = m_functions
+    bc_weak_form = ArbogastDualWeakFormBCDirichlet(fe_space)
+    bc_weak_form.functions = exact_functions
+
+    def scatter_form_data(jac_g, i, weak_form):
+        # destination indexes
+        dest = weak_form.space.destination_indexes(i)
+        alpha_l = alpha[dest]
+        r_el, j_el = weak_form.evaluate_form(i, alpha_l)
+
+        # contribute rhs
+        res_g[dest] += r_el
+
+        # contribute lhs
+        data = j_el.ravel()
+        row = np.repeat(dest, len(dest))
+        col = np.tile(dest, len(dest))
+        nnz = data.shape[0]
+        for k in range(nnz):
+            jac_g.setValue(row=row[k], col=col[k], value=data[k], addv=True)
+
+    def scatter_bc_form(jac_g, i, bc_weak_form):
+        dest = fe_space.bc_destination_indexes(i)
+        alpha_l = alpha[dest]
+        r_el, j_el = bc_weak_form.evaluate_form(i, alpha_l)
+
+        # contribute rhs
+        res_g[dest] += r_el
+
+        # contribute lhs
+        data = j_el.ravel()
+        row = np.repeat(dest, len(dest))
+        col = np.tile(dest, len(dest))
+        nnz = data.shape[0]
+        for k in range(nnz):
+            jac_g.setValue(row=row[k], col=col[k], value=data[k], addv=True)
+
+    # Assembler
+    st = time.time()
+    jac_g = PETSc.Mat()
+    jac_g.createAIJ([n_dof_g, n_dof_g])
+
+    res_g = np.zeros(n_dof_g)
+    print("n_dof: ", n_dof_g)
+
+    # initial guess
+    alpha = np.zeros(n_dof_g)
+
+    for iter in range(n_iterations):
+
+        n_els = len(fe_space.discrete_spaces["mp"].elements)
+        [scatter_form_data(jac_g, i, weak_form) for i in range(n_els)]
+
+        n_bc_els = len(fe_space.discrete_spaces["mp"].bc_elements)
+        [scatter_bc_form(jac_g, i, bc_weak_form) for i in range(n_bc_els)]
+
+        jac_g.assemble()
+
+        et = time.time()
+        elapsed_time = et - st
+        print("Assembly time:", elapsed_time, "seconds")
+
+        res_norm = np.linalg.norm(res_g)
+        stop_criterion_q = res_norm < eps_tol
+        if stop_criterion_q:
+            print("Nonlinear solver converged")
+            print("Residual norm: ", res_norm)
+            print("Number of iterations: ", iter)
+            break
+
+        # solving ls
+        st = time.time()
+        ksp = PETSc.KSP().create()
+        ksp.setOperators(jac_g)
+        b = jac_g.createVecLeft()
+        b.array[:] = -res_g
+        x = jac_g.createVecRight()
+
+        ksp.setType("preonly")
+        ksp.getPC().setType("lu")
+        ksp.getPC().setFactorSolverType("mumps")
+        ksp.setConvergenceHistory()
+        ksp.solve(b, x)
+        delta_alpha = x.array
+
+        et = time.time()
+        elapsed_time = et - st
+        print("Linear solver time:", elapsed_time, "seconds")
+
+        # newton update
+        alpha += delta_alpha
+
+        # Set up to zero lhr and rhs
+        res_g *= 0.0
+        jac_g.scale(0.0)
+
+
+    st = time.time()
+    mp_l2_error, p_l2_error,= l2_error(
+        dim, fe_space, exact_functions, alpha
+    )
+    et = time.time()
+    elapsed_time = et - st
+    print("L2-error time:", elapsed_time, "seconds")
+    print("L2-error in mp: ", mp_l2_error)
+    print("L2-error in p : ", p_l2_error)
+
+
+    if write_vtk_q:
+        # post-process solution
+        st = time.time()
+        file_name = "rates_arbogast_two_fields.vtk"
+        write_vtk_file_with_exact_solution(
+            file_name, gmesh, fe_space, exact_functions, alpha
+        )
+        et = time.time()
+        elapsed_time = et - st
+        print("Post-processing time:", elapsed_time, "seconds")
+
+    return p_l2_error
+
+
+def create_domain(dimension):
+    if dimension == 1:
+        box_points = np.array([[0, 0, 0], [1, 0, 0]])
+        domain = build_box_1D(box_points)
+        return domain
+    elif dimension == 2:
+        box_points = np.array([[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0]])
+        # box_points = [
+        #     point + 0.25 * np.array([-1.0, -1.0, 0.0]) for point in box_points
+        # ]
+        domain = build_box_2D(box_points)
+        return domain
+    else:
+        box_points = np.array(
+            [
+                [0.0, 0.0, 0.0],
+                [1.0, 0.0, 0.0],
+                [1.0, 1.0, 0.0],
+                [0.0, 1.0, 0.0],
+                [0.0, 0.0, 1.0],
+                [1.0, 0.0, 1.0],
+                [1.0, 1.0, 1.0],
+                [0.0, 1.0, 1.0],
+            ]
+        )
+        # box_points = [
+        #     point + 0.25 * np.array([-1.0, -1.0, -1.0]) for point in box_points
+        # ]
+        domain = build_box_3D(box_points)
+        return domain
+
+
+def create_conformal_mesher(domain: Domain, h, ref_l=0):
+    mesher = ConformalMesher(dimension=domain.dimension)
+    mesher.domain = domain
+    mesher.generate_from_domain(h, ref_l)
+    mesher.write_mesh("gmesh.msh")
+    return mesher
+
+
+def create_mesh(dimension, mesher: ConformalMesher, write_vtk_q=False):
+    gmesh = Mesh(dimension=dimension, file_name="gmesh.msh")
+    gmesh.set_conformal_mesher(mesher)
+    gmesh.build_conformal_mesh()
+    if write_vtk_q:
+        gmesh.write_vtk()
+    return gmesh
+
+
+def main():
+    k_order = 1
+    h = 0.25
+    n_ref = 5
+    dimension = 1
+    ref_l = 0
+
+    domain = create_domain(dimension)
+    error_data = np.empty((0, 2), float)
+
+    for method in method_definition(k_order):
+        for l in range(n_ref):
+            h_val = h * (2**-l)
+            mesher = create_conformal_mesher(domain, h_val, 0)
+            gmesh = create_mesh(dimension, mesher, True)
+            error_val = two_fields_formulation(method, gmesh, True)
+            error_data = np.append(error_data, np.array([[h_val, error_val]]), axis=0)
+
+    rates_data = np.empty((0, 1), float)
+    for i in range(error_data.shape[0] - 1):
+        chunk_b = np.log(error_data[i])
+        chunk_e = np.log(error_data[i + 1])
+        h_step = chunk_e[0] - chunk_b[0]
+        partial = (chunk_e - chunk_b) / h_step
+        rates_data = np.append(rates_data, np.array([list(partial[1:2])]), axis=0)
+
+    print("error data: ", error_data)
+    print("error rates data: ", rates_data)
+
+    np.set_printoptions(precision=4)
+    print("rounded error data: ", error_data)
+    print("rounded error rates data: ", rates_data)
+
+    return
+
+    a = error_data[:,0]
+    b = error_data[:,1]
+    plt.loglog(a, b)
+    plt.show()
+    return
+
+
+if __name__ == "__main__":
+    main()
