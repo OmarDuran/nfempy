a1e1d3f06de4d1c8691d0d9ecf917a11ea0f0067
Omar Duran <omaryesiduran@gmail.com>
2024-06-06 13:08:39 +0200

    MAINT: One dimensional plots

--- examples/degerate_elliptic_scalar/plotting_for_Arbogast_problem.py
+++ examples/degerate_elliptic_scalar/plotting_for_Arbogast_problem.py
@@ -3,7 +3,12 @@ from sys import platform
 import os
 import numpy as np
 import pyvista
-from degenerate_mixed_flow_model import compose_case_name, method_definition, material_data_definition, create_domain
+from degenerate_mixed_flow_model import (
+    compose_case_name,
+    method_definition,
+    material_data_definition,
+    create_domain,
+)
 import matplotlib.pyplot as plt
 
 if platform == "linux" or platform == "linux2":
@@ -11,10 +16,7 @@ if platform == "linux" or platform == "linux2":
 pyvista.global_theme.colorbar_orientation = "horizontal"
 
 
-
-
 def paint_scalar_on_canvas_plane(vtk_file_name, scalar_name, title_string):
-
     # Load data from the two VTK files
     pyvista_unstructure_mesh = pyvista.read(vtk_file_name)
 
@@ -49,42 +51,55 @@ def paint_scalar_on_canvas_plane(vtk_file_name, scalar_name, title_string):
     return plotter
 
 
-def plot_over_line(vtk_file_name, scalar_name,  title_string):
-    a = [0.25, 0.25, 0.0]
-    b = [0.75, 0.75, 0.0]
+def plot_over_line(vtk_file_name, scalar_names, title_string, figure_name):
+    a = [-1.0, 0.0, 0.0]
+    b = [1.0, 0.0, 0.0]
     # Load data from the VTK file
     pyvista_unstructured_mesh = pyvista.read(vtk_file_name)
     sampled = pyvista_unstructured_mesh.sample_over_line(a, b, resolution=1000)
 
-    quantity_data = sampled.point_data[scalar_name]
-    quantity_norm = np.array([np.linalg.norm(value) for value in quantity_data])
+    quantities_norm = []
+    for scalar_name in scalar_names:
+        quantity_data = sampled.point_data[scalar_name]
+        quantity_norm = np.array([np.linalg.norm(value) for value in quantity_data])
+        quantities_norm.append(quantity_norm)
 
-
-    x = sampled["Distance"]
+    x = sampled["Distance"] - 1.0
 
     # Plotting
     plt.figure(figsize=(10, 6))
-    lineObjects = plt.plot(x, quantity_norm)
+    lineObjects = plt.plot(x, np.array(quantities_norm).T)
     linewidths = [2.0, 2.0]
     styles = ["solid", "--"]
 
     for i, line in enumerate(lineObjects):
         line.set_linestyle(styles[i])
         line.set_linewidth(linewidths[i])
-    plt.legend(iter(lineObjects), (r"$|| \mathbf{q}_e ||$", r"$|| \mathbf{q}_h ||$"))
+
+    legends = []
+    for scalar_name in scalar_names:
+        legend = r"$|| " + scalar_name + " ||$"
+        legends.append(legend)
+
+    plt.legend(iter(lineObjects), legends)
     plt.title(title_string)
     plt.xlabel("Length")
-    plt.ylabel(scalar_name)
-
-
-    plt.show()
+    plt.savefig(figure_name)
     return
 
 
-
-def two_dimnesional_plots(k_order,source_folder_name,figure_folder_name,figure_format,dimensions,methods,titles_map,filters):
+def two_dimnesional_plots(
+    k_order,
+    source_folder_name,
+    figure_folder_name,
+    figure_format,
+    dimensions,
+    methods,
+    titles_map,
+    filters,
+):
     for method in methods:
-        if method[0] not in filters['method']:
+        if method[0] not in filters["method"]:
             continue
         for dimension in dimensions:
             if dimension == 1 and method[0] == "mixed_bdm":
@@ -94,34 +109,46 @@ def two_dimnesional_plots(k_order,source_folder_name,figure_folder_name,figure_f
             domains = {"fitted": fitted_domain, "unfitted": unfitted_domain}
             materials = material_data_definition(dimension)
             for domain in domains.items():
-                if domain[0] not in filters['domain_type']:
+                if domain[0] not in filters["domain_type"]:
                     continue
                 for material in materials:
-                    if material['m_par'] not in filters['parameter']:
+                    if material["m_par"] not in filters["parameter"]:
                         continue
                     case_name = compose_case_name(
                         method, dimension, domain, material, source_folder_name
                     )
-                    vtk_suffix = 'l_' + str(filters['l']) + filters['suffix']
+                    vtk_suffix = "l_" + str(filters["l"]) + filters["suffix"]
                     vtk_file_name = case_name + vtk_suffix
 
                     if not os.path.exists(figure_folder_name):
                         os.makedirs(figure_folder_name)
 
-                    title_string = titles_map[filters['scalar_name']]
-                    canvas = paint_scalar_on_canvas_plane(vtk_file_name, filters['scalar_name'], title_string)
+                    title_string = titles_map[filters["scalar_name"]]
+                    canvas = paint_scalar_on_canvas_plane(
+                        vtk_file_name, filters["scalar_name"], title_string
+                    )
                     figure_case_name = compose_case_name(
                         method, dimension, domain, material, figure_folder_name
                     )
-                    figure_suffix = filters['scalar_name'] + '_magnitude.' + figure_format
+                    figure_suffix = (
+                        filters["scalar_name"] + "_magnitude." + figure_format
+                    )
                     figure_name = figure_case_name + figure_suffix
                     canvas.save_graphic(figure_name)
 
 
-
-def one_dimnesional_plots(k_order,source_folder_name,figure_folder_name,figure_format,dimensions,methods,titles_map,filters):
+def one_dimnesional_plots(
+    k_order,
+    source_folder_name,
+    figure_folder_name,
+    figure_format,
+    dimensions,
+    methods,
+    titles_map,
+    filters,
+):
     for method in methods:
-        if method[0] not in filters['method']:
+        if method[0] not in filters["method"]:
             continue
         for dimension in dimensions:
             if dimension == 1 and method[0] == "mixed_bdm":
@@ -131,70 +158,89 @@ def one_dimnesional_plots(k_order,source_folder_name,figure_folder_name,figure_f
             domains = {"fitted": fitted_domain, "unfitted": unfitted_domain}
             materials = material_data_definition(dimension)
             for domain in domains.items():
-                if domain[0] not in filters['domain_type']:
+                if domain[0] not in filters["domain_type"]:
                     continue
                 for material in materials:
-                    if material['m_par'] not in filters['parameter']:
+                    if material["m_par"] not in filters["parameter"]:
                         continue
                     case_name = compose_case_name(
                         method, dimension, domain, material, source_folder_name
                     )
-                    vtk_suffix = 'l_' + str(filters['l']) + filters['suffix']
+                    vtk_suffix = "l_" + str(filters["l"]) + filters["suffix"]
                     vtk_file_name = case_name + vtk_suffix
 
                     if not os.path.exists(figure_folder_name):
                         os.makedirs(figure_folder_name)
 
-                    title_string = titles_map[filters['scalar_name']]
-                   # canvas = paint_scalar_on_canvas_plane(vtk_file_name, filters['scalar_name'], title_string)
+                    title_string = titles_map[filters["scalar_names"][0]]
                     figure_case_name = compose_case_name(
                         method, dimension, domain, material, figure_folder_name
                     )
-                    figure_suffix = filters['scalar_name'] + '_magnitude.' + figure_format
-                    figure_name = figure_case_name + figure_suffix
-                    #canvas.save_graphic(figure_name)
-                    # if dimension == 1:
-                    plot_over_line(vtk_file_name, filters['scalar_name'], title_string)
 
+                    scalars_chunk = ""
+                    for scalar_name in filters["scalar_names"]:
+                        scalars_chunk += scalar_name
+                        scalars_chunk += "_"
+
+                    figure_suffix = scalars_chunk + "magnitude." + figure_format
+                    figure_name = figure_case_name + figure_suffix
+                    plot_over_line(
+                        vtk_file_name,
+                        filters["scalar_names"],
+                        title_string,
+                        figure_name,
+                    )
 
 
 k_order = 0
 source_folder_name = "output"
 figure_folder_name = "Arbogast_figures"
-figure_format = 'eps'
+figure_format = "eps"
 dimensions = [1]
 methods = method_definition(k_order)
-titles_map = {'p_h': 'Physical pressure', 'q_h': 'Unphysical pressure', 'u_h': 'Physical velocity norm','v_h': 'Unphysical velocity norm', 'p_error': 'l2-error in physical pressure', 'q_error': 'l2-error in unphysical pressure','u_error':'l2-error in physical velocity','v_error':'l2-error in unphysical velocity'}
+titles_map = {
+    "p_h": "Physical pressure",
+    "p_e": "Physical pressure",
+    "q_h": "Unphysical pressure",
+    "q_e": "Unphysical pressure",
+    "u_h": "Physical velocity norm",
+    "u_e": "Physical velocity norm",
+    "v_h": "Unphysical velocity norm",
+    "v_e": "Unphysical velocity norm",
+    "p_error": "l2-error in physical pressure",
+    "q_error": "l2-error in unphysical pressure",
+    "u_error": "l2-error in physical velocity",
+    "v_error": "l2-error in unphysical velocity",
+}
 
-# to filter
-filters = {'method': ['mixed_rt'], 'l': 5, 'suffix': '_physical_two_fields_l2_error.vtk', 'domain_type': ['fitted', 'unfitted'], 'parameter': [0.5],
-           'scalar_name': 'u_error'}
+# Filters for 2d
+filters_2d = {
+    "method": ["mixed_rt"],
+    "l": 5,
+    "suffix": "_physical_two_fields_l2_error.vtk",
+    "domain_type": ["fitted", "unfitted"],
+    "parameter": [0.5],
+    "scalar_name": "u_error",
+}
+# two_dimnesional_plots(k_order,source_folder_name,figure_folder_name,figure_format,dimensions,methods,titles_map,filters_2d)
 
-one_dimnesional_plots(k_order,source_folder_name,figure_folder_name,figure_format,dimensions,methods,titles_map,filters)
-
-
-
-
-
-
-# q_h_norm = np.array([np.linalg.norm(q_h) for q_h in sampled.point_data["q_h"]])
-# q_e_norm = np.array([np.linalg.norm(q_e) for q_e in sampled.point_data["q_e"]])
-
-# x = sampled["Distance"]
-# q_e = quantity_norm
-# q_data = np.vstack((q_e_norm, q_h_norm)).T
-# lineObjects = plt.plot(x, q_data)
-# styles = ["solid", "--"]
-# linewidths = [2.0, 2.0]
-# for i, line in enumerate(lineObjects):
-#     line.set_linestyle(styles[i])
-#     line.set_linewidth(linewidths[i])
-# plt.legend(iter(lineObjects), (r"$|| \mathbf{q}_e ||$", r"$|| \mathbf{q}_h ||$"))
-# plt.title("")
-# plt.xlabel("Length")
-# plt.ylabel("Flux")
-#  plt.show()
-#
-# plt.savefig(figure_file_name)
-#
-# return
+# Filters for 1d
+figure_format_1d = "pdf"
+filters_1d = {
+    "method": ["mixed_rt"],
+    "l": 7,
+    "suffix": "_two_fields.vtk",
+    "domain_type": ["fitted", "unfitted"],
+    "parameter": [0.5],
+    "scalar_names": ["v_h", "v_e"],
+}
+one_dimnesional_plots(
+    k_order,
+    source_folder_name,
+    figure_folder_name,
+    figure_format_1d,
+    dimensions,
+    methods,
+    titles_map,
+    filters_1d,
+)
