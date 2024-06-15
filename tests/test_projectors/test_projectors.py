a8b2808f4f1b330aedf1b1a4d880dba5468df357
Omar Duran <omaryesiduran@gmail.com>
2024-06-15 16:06:58 +0200

    Update test_projectors.py

--- tests/test_projectors/test_projectors.py
+++ tests/test_projectors/test_projectors.py
@@ -125,7 +125,7 @@ def test_scalar_h1_projector(k_order):
             space.make_subspaces_discontinuous(discrete_spaces_disc)
             space.build_structures(discrete_spaces_bc_physical_tags)
 
-            alpha = l2_projector(space, exact_functions)
+            alpha = l2_projector(space, exact_functions, False)
             error_val = l2_error(dim, space, exact_functions, alpha)
             l2_error_q = np.any(np.isclose(np.array(error_val), 0.0, atol=1.0e-13))
             assert l2_error_q
@@ -167,7 +167,7 @@ def test_vector_hdiv_projector(k_order):
                 space.make_subspaces_discontinuous(discrete_spaces_disc)
                 space.build_structures(discrete_spaces_bc_physical_tags)
 
-                alpha = l2_projector(space, exact_functions)
+                alpha = l2_projector(space, exact_functions, False)
                 error_val = l2_error(dim, space, exact_functions, alpha)
                 l2_error_q = np.any(np.isclose(np.array(error_val), 0.0, atol=1.0e-13))
                 assert l2_error_q
@@ -209,7 +209,7 @@ def test_vector_hcurl_projector(k_order):
                 space.make_subspaces_discontinuous(discrete_spaces_disc)
                 space.build_structures(discrete_spaces_bc_physical_tags)
 
-                alpha = l2_projector(space, exact_functions)
+                alpha = l2_projector(space, exact_functions, False)
                 error_val = l2_error(dim, space, exact_functions, alpha)
                 l2_error_q = np.any(np.isclose(np.array(error_val), 0.0, atol=1.0e-13))
                 assert l2_error_q
