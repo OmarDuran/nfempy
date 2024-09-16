195a32159687f4fb5b88954d7d82112319d345dd
Omar Duran <omaryesiduran@gmail.com>
2024-09-16 12:46:41 +0200

    WIP: weakly coupled methods

--- examples/cosserat_elasticity/strong_solution_cosserat_elasticity_example_1.py
+++ examples/cosserat_elasticity/strong_solution_cosserat_elasticity_example_1.py
@@ -206,7 +206,14 @@ def couple_stress(material_data, dim: int = 2):
         )
 
 
-def couple_stress_scaled(m_lambda, m_mu, m_kappa, m_gamma, dim: int = 2):
+def couple_stress_scaled(material_data, dim: int = 2):
+
+    m_lambda_o = material_data["lambda_o"]
+    m_mu_o = material_data["mu_o"]
+    m_kappa_o = material_data["kappa_o"]
+
+    m_l = material_data["l"]
+
     if dim == 2:
         return lambda x, y, z: m_gamma * np.array(
             [
@@ -217,25 +224,22 @@ def couple_stress_scaled(m_lambda, m_mu, m_kappa, m_gamma, dim: int = 2):
             ]
         )
     else:
-        return lambda x, y, z: m_gamma * np.array(
+        return lambda x, y, z: np.array(
             [
                 [
-                    (1 - x) * np.sin(np.pi * y) * np.sin(np.pi * z)
-                    - x * np.sin(np.pi * y) * np.sin(np.pi * z),
-                    np.pi * (1 - x) * x * np.cos(np.pi * y) * np.sin(np.pi * z),
-                    np.pi * (1 - x) * x * np.cos(np.pi * z) * np.sin(np.pi * y),
+                    -((-1 + 2*x)*(m_lambda_o + 2*m_mu_o)*np.sin(np.pi*y)*np.sin(np.pi*z)) + m_lambda_o*np.sin(np.pi*x)*((1 - 2*z)*np.sin(np.pi*y) + (1 - 2*y)*np.sin(np.pi*z)),
+                    np.pi*((-1 + y)*y*(m_kappa_o - m_mu_o)*np.cos(np.pi*x) - (-1 + x)*x*(m_kappa_o + m_mu_o)*np.cos(np.pi*y))*np.sin(np.pi*z),
+                    np.pi*((-1 + z)*z*(m_kappa_o - m_mu_o)*np.cos(np.pi*x) - (-1 + x)*x*(m_kappa_o + m_mu_o)*np.cos(np.pi*z))*np.sin(np.pi*y),
                 ],
                 [
-                    np.pi * (1 - y) * y * np.cos(np.pi * x) * np.sin(np.pi * z),
-                    (1 - y) * np.sin(np.pi * x) * np.sin(np.pi * z)
-                    - y * np.sin(np.pi * x) * np.sin(np.pi * z),
-                    np.pi * (1 - y) * y * np.cos(np.pi * z) * np.sin(np.pi * x),
+                    np.pi*(-((-1 + y)*y*(m_kappa_o + m_mu_o)*np.cos(np.pi*x)) + (-1 + x)*x*(m_kappa_o - m_mu_o)*np.cos(np.pi*y))*np.sin(np.pi*z),
+                    (1 - 2*x)*m_lambda_o*np.sin(np.pi*y)*np.sin(np.pi*z) + np.sin(np.pi*x)*((m_lambda_o - 2*z*m_lambda_o)*np.sin(np.pi*y) - (-1 + 2*y)*(m_lambda_o + 2*m_mu_o)*np.sin(np.pi*z)),
+                    np.pi*((-1 + z)*z*(m_kappa_o - m_mu_o)*np.cos(np.pi*y) - (-1 + y)*y*(m_kappa_o + m_mu_o)*np.cos(np.pi*z))*np.sin(np.pi*x),
                 ],
                 [
-                    np.pi * (1 - z) * z * np.cos(np.pi * x) * np.sin(np.pi * y),
-                    np.pi * (1 - z) * z * np.cos(np.pi * y) * np.sin(np.pi * x),
-                    (1 - z) * np.sin(np.pi * x) * np.sin(np.pi * y)
-                    - z * np.sin(np.pi * x) * np.sin(np.pi * y),
+                    np.pi*(-((-1 + z)*z*(m_kappa_o + m_mu_o)*np.cos(np.pi*x)) + (-1 + x)*x*(m_kappa_o - m_mu_o)*np.cos(np.pi*z))*np.sin(np.pi*y),
+                    np.pi*(-((-1 + z)*z*(m_kappa_o + m_mu_o)*np.cos(np.pi*y)) + (-1 + y)*y*(m_kappa_o - m_mu_o)*np.cos(np.pi*z))*np.sin(np.pi*x),
+                    (1 - 2*x)*m_lambda_o*np.sin(np.pi*y)*np.sin(np.pi*z) + np.sin(np.pi*x)*(-((-1 + 2*z)*(m_lambda_o + 2*m_mu_o)*np.sin(np.pi*y)) + (1 - 2*y)*m_lambda_o*np.sin(np.pi*z)),
                 ],
             ]
         )
@@ -359,7 +363,18 @@ def rhs(material_data, dim: int = 2):
         )
 
 
-def rhs_scaled(m_lambda, m_mu, m_kappa, m_gamma, dim: int = 2):
+def rhs_scaled(material_data, dim: int = 2):
+
+    m_lambda_s = material_data["lambda_s"]
+    m_mu_s = material_data["mu_s"]
+    m_kappa_s = material_data["kappa_s"]
+
+    m_lambda_o = material_data["lambda_o"]
+    m_mu_o = material_data["mu_o"]
+    m_kappa_o = material_data["kappa_o"]
+
+    m_l = material_data["l"]
+
     if dim == 2:
         return lambda x, y, z: np.array(
             [
@@ -396,140 +411,73 @@ def rhs_scaled(m_lambda, m_mu, m_kappa, m_gamma, dim: int = 2):
     else:
         return lambda x, y, z: np.array(
             [
-                -2 * (np.pi**2) * (-1 + y) * y * (-1 + z) * z * m_mu * np.sin(np.pi * x)
-                + (-1 + z)
-                * z
-                * (
-                    -(np.pi * (-1 + 2 * x) * (m_kappa - m_mu) * np.cos(np.pi * y))
-                    + 2
-                    * (m_kappa + m_mu + np.pi * m_kappa * np.cos(np.pi * y))
-                    * np.sin(np.pi * x)
-                )
-                + np.pi
-                * m_lambda
-                * (
-                    (-1 + 2 * x) * (-1 + z) * z * np.cos(np.pi * y)
-                    + (-1 + y)
-                    * y
-                    * (
-                        (-1 + 2 * x) * np.cos(np.pi * z)
-                        - np.pi * (-1 + z) * z * np.sin(np.pi * x)
-                    )
-                )
-                + (-1 + y)
-                * y
-                * (
-                    2 * (m_kappa + m_mu) * np.sin(np.pi * x)
-                    - np.pi
-                    * np.cos(np.pi * z)
-                    * (
-                        (-1 + 2 * x) * (m_kappa - m_mu)
-                        + 2 * m_kappa * np.sin(np.pi * x)
-                    )
-                ),
-                -2 * (np.pi**2) * (-1 + x) * x * (-1 + z) * z * m_mu * np.sin(np.pi * y)
-                + (-1 + x)
-                * x
-                * (
-                    -(np.pi * (-1 + 2 * y) * (m_kappa - m_mu) * np.cos(np.pi * z))
-                    + 2
-                    * (m_kappa + m_mu + np.pi * m_kappa * np.cos(np.pi * z))
-                    * np.sin(np.pi * y)
-                )
-                + np.pi
-                * m_lambda
-                * (
-                    (-1 + 2 * y) * (-1 + z) * z * np.cos(np.pi * x)
-                    + (-1 + x)
-                    * x
-                    * (
-                        (-1 + 2 * y) * np.cos(np.pi * z)
-                        - np.pi * (-1 + z) * z * np.sin(np.pi * y)
-                    )
-                )
-                - (-1 + z)
-                * z
-                * (
-                    -2 * (m_kappa + m_mu) * np.sin(np.pi * y)
-                    + np.pi
-                    * np.cos(np.pi * x)
-                    * (
-                        (-1 + 2 * y) * (m_kappa - m_mu)
-                        + 2 * m_kappa * np.sin(np.pi * y)
-                    )
-                ),
-                -2 * (np.pi**2) * (-1 + x) * x * (-1 + y) * y * m_mu * np.sin(np.pi * z)
-                + np.pi
-                * m_lambda
-                * (
-                    (-1 + y) * y * (-1 + 2 * z) * np.cos(np.pi * x)
-                    + (-1 + x)
-                    * x
-                    * (
-                        (-1 + 2 * z) * np.cos(np.pi * y)
-                        - np.pi * (-1 + y) * y * np.sin(np.pi * z)
-                    )
-                )
-                - (-1 + x)
-                * x
-                * (
-                    -2 * (m_kappa + m_mu) * np.sin(np.pi * z)
-                    + np.pi
-                    * np.cos(np.pi * y)
-                    * (
-                        (-1 + 2 * z) * (m_kappa - m_mu)
-                        + 2 * m_kappa * np.sin(np.pi * z)
-                    )
-                )
-                + (-1 + y)
-                * y
-                * (
-                    2 * (m_kappa + m_mu) * np.sin(np.pi * z)
-                    + np.pi
-                    * np.cos(np.pi * x)
-                    * (
-                        m_kappa
-                        - 2 * z * m_kappa
-                        - m_mu
-                        + 2 * z * m_mu
-                        + 2 * m_kappa * np.sin(np.pi * z)
-                    )
-                ),
-                -2 * (-1 + x) * x * (-1 + 2 * y) * m_kappa * np.sin(np.pi * z)
-                + 2
-                * np.sin(np.pi * y)
-                * (
-                    (-1 + x) * x * (-1 + 2 * z) * m_kappa
-                    + (
-                        (-1 + (np.pi**2) * (-1 + x) * x) * m_gamma * m_gamma
-                        + 2 * (-1 + x) * x * m_kappa
-                    )
-                    * np.sin(np.pi * z)
-                ),
-                2
-                * (
-                    (-1 + 2 * x) * (-1 + y) * y * m_kappa * np.sin(np.pi * z)
-                    + np.sin(np.pi * x)
-                    * (
-                        y * (-1 + y + 2 * z - 2 * y * z) * m_kappa
-                        + (
-                            (-1 + (np.pi**2) * (-1 + y) * y) * m_gamma * m_gamma
-                            + 2 * (-1 + y) * y * m_kappa
-                        )
-                        * np.sin(np.pi * z)
-                    )
-                ),
-                -2 * (-1 + 2 * x) * (-1 + z) * z * m_kappa * np.sin(np.pi * y)
-                + 2
-                * np.sin(np.pi * x)
-                * (
-                    (-1 + 2 * y) * (-1 + z) * z * m_kappa
-                    + (
-                        (-1 + (np.pi**2) * (-1 + z) * z) * m_gamma * m_gamma
-                        + 2 * (-1 + z) * z * m_kappa
-                    )
-                    * np.sin(np.pi * y)
-                ),
+                -2 * (np.pi ** 2) * (-1 + y) * y * (-1 + z) * z * m_mu_s * np.sin(
+                    np.pi * x) + (-1 + z) * z * (-(
+                        np.pi * (-1 + 2 * x) * (m_kappa_s - m_mu_s) * np.cos(
+                    np.pi * y)) + 2 * (
+                                                         m_kappa_s + m_mu_s + np.pi * m_kappa_s * np.cos(
+                                                     np.pi * y)) * np.sin(
+                    np.pi * x)) +
+                np.pi * m_lambda_s * ((-1 + 2 * x) * (-1 + z) * z * np.cos(np.pi * y) + (
+                        -1 + y) * y * ((-1 + 2 * x) * np.cos(np.pi * z) - np.pi * (
+                        -1 + z) * z * np.sin(np.pi * x))) + (-1 + y) * y * (
+                        2 * (m_kappa_s + m_mu_s) * np.sin(np.pi * x) - np.pi * np.cos(
+                    np.pi * z) * ((-1 + 2 * x) * (
+                        m_kappa_s - m_mu_s) + 2 * m_kappa_s * np.sin(np.pi * x))),
+                -2 * (np.pi ** 2) * (-1 + x) * x * (-1 + z) * z * m_mu_s * np.sin(
+                    np.pi * y) + (-1 + x) * x * (-(
+                        np.pi * (-1 + 2 * y) * (m_kappa_s - m_mu_s) * np.cos(
+                    np.pi * z)) + 2 * (
+                                                         m_kappa_s + m_mu_s + np.pi * m_kappa_s * np.cos(
+                                                     np.pi * z)) * np.sin(
+                    np.pi * y)) +
+                np.pi * m_lambda_s * ((-1 + 2 * y) * (-1 + z) * z * np.cos(np.pi * x) + (
+                        -1 + x) * x * ((-1 + 2 * y) * np.cos(np.pi * z) - np.pi * (
+                        -1 + z) * z * np.sin(np.pi * y))) - (-1 + z) * z * (
+                        -2 * (m_kappa_s + m_mu_s) * np.sin(
+                    np.pi * y) + np.pi * np.cos(np.pi * x) * ((-1 + 2 * y) * (
+                        m_kappa_s - m_mu_s) + 2 * m_kappa_s * np.sin(np.pi * y))),
+                -2 * (np.pi ** 2) * (-1 + x) * x * (-1 + y) * y * m_mu_s * np.sin(
+                    np.pi * z) + np.pi * m_lambda_s * (
+                        (-1 + y) * y * (-1 + 2 * z) * np.cos(np.pi * x) + (
+                        -1 + x) * x * (
+                                (-1 + 2 * z) * np.cos(np.pi * y) - np.pi * (
+                                -1 + y) * y * np.sin(np.pi * z))) -
+                (-1 + x) * x * (-2 * (m_kappa_s + m_mu_s) * np.sin(
+                    np.pi * z) + np.pi * np.cos(np.pi * y) * ((-1 + 2 * z) * (
+                        m_kappa_s - m_mu_s) + 2 * m_kappa_s * np.sin(np.pi * z))) + (
+                        -1 + y) * y * (
+                        2 * (m_kappa_s + m_mu_s) * np.sin(np.pi * z) + np.pi * np.cos(
+                    np.pi * x) * (
+                                m_kappa_s - 2 * z * m_kappa_s - m_mu_s + 2 * z * m_mu_s + 2 * m_kappa_s * np.sin(
+                            np.pi * z))),
+                -((-1 + 2 * y) * (2 * (-1 + x) * x * m_kappa_s - (m_l ** 2) * np.pi * (
+                            m_kappa_o - m_lambda_o - m_mu_o) * np.cos(
+                    np.pi * x)) * np.sin(np.pi * z)) +
+                np.sin(np.pi * y) * (2 * (-1 + x) * x * (-1 + 2 * z) * m_kappa_s + (
+                            m_l ** 2) * np.pi * (-1 + 2 * z) * (
+                                                 m_kappa_o - m_lambda_o - m_mu_o) * np.cos(
+                    np.pi * x) + 2 * (2 * (-1 + x) * x * m_kappa_s + (m_l ** 2) * (
+                            -m_lambda_o - 2 * m_mu_o + (np.pi ** 2) * (-1 + x) * x * (
+                                m_kappa_o + m_mu_o))) * np.sin(np.pi * z)),
+                (-1 + 2 * x) * (2 * (-1 + y) * y * m_kappa_s + (m_l ** 2) * np.pi * (
+                            m_kappa_o - m_lambda_o - m_mu_o) * np.cos(
+                    np.pi * y)) * np.sin(np.pi * z) +
+                np.sin(np.pi * x) * (2 * y * (-1 + y + 2 * z - 2 * y * z) * m_kappa_s + (
+                            m_l ** 2) * np.pi * (-1 + 2 * z) * (
+                                                 m_kappa_o - m_lambda_o - m_mu_o) * np.cos(
+                    np.pi * y) + 2 * (2 * (-1 + y) * y * m_kappa_s + (m_l ** 2) * (
+                            -m_lambda_o - 2 * m_mu_o + (np.pi ** 2) * (-1 + y) * y * (
+                                m_kappa_o + m_mu_o))) * np.sin(np.pi * z)),
+                -((-1 + 2 * x) * (2 * (-1 + z) * z * m_kappa_s - (m_l ** 2) * np.pi * (
+                            m_kappa_o - m_lambda_o - m_mu_o) * np.cos(
+                    np.pi * z)) * np.sin(np.pi * y)) +
+                np.sin(np.pi * x) * (2 * (-1 + 2 * y) * (-1 + z) * z * m_kappa_s + (
+                            m_l ** 2) * np.pi * (-1 + 2 * y) * (
+                                                 m_kappa_o - m_lambda_o - m_mu_o) * np.cos(
+                    np.pi * z) + 2 * (2 * (-1 + z) * z * m_kappa_s + (m_l ** 2) * (
+                            -m_lambda_o - 2 * m_mu_o + (np.pi ** 2) * (-1 + z) * z * (
+                                m_kappa_o + m_mu_o))) * np.sin(np.pi * y)),
             ]
         )
 
@@ -667,7 +615,13 @@ def couple_stress_divergence(material_data, dim: int = 2):
         )
 
 
-def couple_stress_divergence_scaled(m_lambda, m_mu, m_kappa, m_gamma, dim: int = 2):
+def couple_stress_divergence_scaled(material_data, dim: int = 2):
+
+    m_lambda_o = material_data["lambda_o"]
+    m_mu_o = material_data["mu_o"]
+    m_kappa_o = material_data["kappa_o"]
+
+    m_l = material_data["l"]
     if dim == 2:
         return lambda x, y, z: np.array(
             [
@@ -682,29 +636,48 @@ def couple_stress_divergence_scaled(m_lambda, m_mu, m_kappa, m_gamma, dim: int =
     else:
         return lambda x, y, z: np.array(
             [
-                2
-                * (-1 + (np.pi**2) * (-1 + x) * x)
-                * m_gamma
-                * m_gamma
-                * np.sin(np.pi * y)
-                * np.sin(np.pi * z),
-                2
-                * (-1 + (np.pi**2) * (-1 + y) * y)
-                * m_gamma
-                * m_gamma
-                * np.sin(np.pi * x)
-                * np.sin(np.pi * z),
-                2
-                * (-1 + (np.pi**2) * (-1 + z) * z)
-                * m_gamma
-                * m_gamma
-                * np.sin(np.pi * x)
-                * np.sin(np.pi * y),
+                (m_l ** 2) * (
+                            np.pi * (1 - 2 * z) * m_lambda_o * np.cos(np.pi * x) * np.sin(
+                        np.pi * y) + np.pi * (-1 + 2 * z) * (m_kappa_o - m_mu_o) * np.cos(
+                        np.pi * x) * np.sin(np.pi * y) + np.pi * (
+                                        1 - 2 * y) * m_lambda_o * np.cos(
+                        np.pi * x) * np.sin(np.pi * z) + np.pi * (-1 + 2 * y) * (
+                                        m_kappa_o - m_mu_o) * np.cos(np.pi * x) * np.sin(
+                        np.pi * z) +
+                            2 * (np.pi ** 2) * (-1 + x) * x * (
+                                        m_kappa_o + m_mu_o) * np.sin(np.pi * y) * np.sin(
+                        np.pi * z) - 2 * (m_lambda_o + 2 * m_mu_o) * np.sin(
+                        np.pi * y) * np.sin(np.pi * z)),
+                (m_l ** 2) * (np.pi * (m_lambda_o - 2 * z * m_lambda_o) * np.cos(
+                    np.pi * y) * np.sin(np.pi * x) + np.pi * (-1 + 2 * z) * (
+                                          m_kappa_o - m_mu_o) * np.cos(
+                    np.pi * y) * np.sin(np.pi * x) + np.pi * (
+                                          1 - 2 * x) * m_lambda_o * np.cos(
+                    np.pi * y) * np.sin(np.pi * z) + np.pi * (-1 + 2 * x) * (
+                                          m_kappa_o - m_mu_o) * np.cos(
+                    np.pi * y) * np.sin(np.pi * z) +
+                              2 * (np.pi ** 2) * (-1 + y) * y * (
+                                          m_kappa_o + m_mu_o) * np.sin(
+                            np.pi * x) * np.sin(np.pi * z) - 2 * (
+                                          m_lambda_o + 2 * m_mu_o) * np.sin(
+                            np.pi * x) * np.sin(np.pi * z)),
+                (m_l ** 2) * (
+                            np.pi * (1 - 2 * y) * m_lambda_o * np.cos(np.pi * z) * np.sin(
+                        np.pi * x) + np.pi * (-1 + 2 * y) * (m_kappa_o - m_mu_o) * np.cos(
+                        np.pi * z) * np.sin(np.pi * x) + np.pi * (
+                                        1 - 2 * x) * m_lambda_o * np.cos(
+                        np.pi * z) * np.sin(np.pi * y) + np.pi * (-1 + 2 * x) * (
+                                        m_kappa_o - m_mu_o) * np.cos(np.pi * z) * np.sin(
+                        np.pi * y) +
+                            2 * (np.pi ** 2) * (-1 + z) * z * (
+                                        m_kappa_o + m_mu_o) * np.sin(np.pi * x) * np.sin(
+                        np.pi * y) - 2 * (m_lambda_o + 2 * m_mu_o) * np.sin(
+                        np.pi * x) * np.sin(np.pi * y)),
             ]
         )
 
 
-def get_material_functions(material_data):
+def get_material_functions(material_data, dim: int = 2):
 
     m_lambda_s = material_data["lambda_s"]
     m_mu_s = material_data["mu_s"]
@@ -733,6 +706,20 @@ def get_material_functions(material_data):
     def f_l(x, y, z):
         return m_l * np.ones_like(x)
 
+    if dim == 2:
+        def f_grad_l(x, y, z):
+            d_gamma_x = 0.0 * x
+            d_gamma_y = 0.0 * y
+            return np.array([d_gamma_x, d_gamma_y])
+    elif dim == 3:
+        def f_grad_l(x, y, z):
+            d_gamma_x = 0.0 * x
+            d_gamma_y = 0.0 * y
+            d_gamma_z = 0.0 * z
+            return np.array([d_gamma_x, d_gamma_y, d_gamma_z])
+    else:
+        raise ValueError("Dimension not implemented: ", dim)
+
     m_functions = {
         "lambda_s": f_lambda_s,
         "mu_s": f_mu_s,
@@ -741,6 +728,7 @@ def get_material_functions(material_data):
         "mu_o": f_mu_o,
         "kappa_o": f_kappa_o,
         "l": f_l,
+        "grad_l": f_grad_l,
     }
 
     return m_functions
