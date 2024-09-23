c4dcc357bb7d7672b390802ce83621d604ee4d7a
Omar Duran <omaryesiduran@gmail.com>
2024-09-23 15:41:17 +0200

    Update compute_results.sh

--- examples/cosserat_elasticity/compute_results.sh
+++ examples/cosserat_elasticity/compute_results.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
-
-# echo " "
-# echo "Example 3:: started"
-# python -OO mfe_cosserat_elasticity_example_3.py
-# mkdir output_example_3
-# mv wc_rt* output_example_3/
-# mv wc_bdm* output_example_3/
-# mv geometric_* output_example_3/
-# echo "Example 3:: completed"
-
-
-# echo " "
-# echo "Example 1:: started"
-# python -OO mfe_cosserat_elasticity_example_1.py
-# mkdir output_example_1
-# mv sc_rt* output_example_1/
-# mv sc_bdm* output_example_1/
-# mv wc_rt* output_example_1/
-# mv wc_bdm* output_example_1/
-# mv geometric_* output_example_1/
-# echo "Example 1:: completed"
+echo " "
+echo "Example 1:: started"
+python -OO mfe_cosserat_elasticity_example_1.py
+mkdir output_example_1
+mv sc_rt* output_example_1/
+mv sc_bdm* output_example_1/
+mv wc_rt* output_example_1/
+mv wc_bdm* output_example_1/
+mv geometric_* output_example_1/
+echo "Example 1:: completed"
 
 
 echo " "
@@ -35,8 +24,11 @@ mv geometric_* output_example_2/
 echo "Example 2:: completed"
 
 
-
-
-
-
-
+echo " "
+echo "Example 3:: started"
+python -OO mfe_cosserat_elasticity_example_3.py
+mkdir output_example_3
+mv wc_rt* output_example_3/
+mv wc_bdm* output_example_3/
+mv geometric_* output_example_3/
+echo "Example 3:: completed"
